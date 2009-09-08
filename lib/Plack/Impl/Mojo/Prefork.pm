c44d07331b5d8a4575bcd38d2a2a0f8a65452054
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2009-09-09 06:03:45 +0900

    oops, forgot to add Mojo::Prefork.
    Also, renamed tests 00_*.t to just *.t. Sorry I don't like it.

---
+++ lib/Plack/Impl/Mojo/Prefork.pm
@@ -0,0 +1,24 @@
+package Plack::Impl::Mojo::Prefork;
+use strict;
+use base qw(Plack::Impl::Mojo);
+sub mojo_daemon_class { 'Mojo::Server::Daemon::Prefork' }
+
+1;
+
+__END__
+
+=head1 NAME
+
+Plack::Impl::Mojo::Prefork - Use Mojo's prefork server
+
+=head1 SYNOPSIS
+
+  use Plack::Impl::Mojo::Prefork;
+
+  my $server = Plack::Impl::Mojo::Prefork->new(
+      host => $host,
+      port => $port,
+  );
+  $server->run($app);
+
+=cut
