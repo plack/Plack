10713deff3306248109200e3d821bea98fbe1328
mala <github@ma.la>
2009-09-08 20:29:24 +0900

    change API for loader

--- lib/Plack/Impl/FCGI/EV.pm
+++ lib/Plack/Impl/FCGI/EV.pm
@@ -4,58 +4,40 @@ use strict;
 use warnings;
 
 use Plack::Util;
-use Any::Moose;
-use Plack::Util;
 use EV;
 use FCGI::EV;
 use Plack::Impl::FCGI::EV::Handler;
-
-has host => (
-    is      => 'ro',
-    isa     => 'Str',
-    default => '127.0.0.1',
-    trigger => sub {
-        my $self = shift;
-        utf8::downgrade( $self->{host} );
-    },
-);
-
-has port => (
-    is      => 'ro',
-    isa     => 'Int',
-    default => 1978,
-    trigger => sub {
-        my $self = shift;
-        utf8::downgrade( $self->{port} );
-    },
-);
-
-has psgi_app => (
-    is  => 'rw',
-    isa => 'CodeRef',
-);
-
 use Data::Dumper;
 
+sub new {
+    my ( $class, %args ) = @_;
+
+    my $self = bless {}, $class;
+    $self->{host} = delete $args{host} || undef;
+    $self->{port} = delete $args{port} || undef;
+
+    $self;
+}
+
 sub run {
-    my $self = shift;
+    my ($self, $app) = @_;
     my $sock = IO::Socket::INET->new(
-        LocalAddr => $self->host,
-        LocalPort => $self->port,
+        LocalAddr => $self->{host},
+        LocalPort => $self->{port},
         ReuseAddr => 1,
         Proto     => 'tcp',
         Listen    => 10,
         Blocking  => 0,
     ) or die "cannot open fcgi server";
     
-    $ENV{SERVER_PORT} = $self->port,
-    $ENV{SERVER_NAME} = $self->host,
+    $ENV{SERVER_PORT} = $self->{port},
+    $ENV{SERVER_NAME} = $self->{host},
     # warn Dumper \%ENV;
 
     # warn $sock;
 
     my $handler_class = 'Plack::Impl::FCGI::EV::Handler';
-    $handler_class->psgi_app($self->psgi_app);
+    $handler_class->psgi_app($app);
     my $w = EV::io $sock, EV::READ, sub {
         my $client = $sock->accept;
         # warn $client;
@@ -67,6 +49,10 @@ sub run {
     $self->{_sock} = $sock;
     $self->{_guard} = $w;
     return $self;
+}
+
+sub run_loop {
+    EV::loop;
 }
 
 1;
