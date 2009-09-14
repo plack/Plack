8cc15dae97c197d8e25e34cfbf63dcfbd1b428ad
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2009-09-14 21:16:59 +0900

    Apache2 backend. 1800 req/s for static files: slower than Standalone :)

---
+++ lib/Plack/Impl/Apache2.pm
@@ -0,0 +1,81 @@
+package Plack::Impl::Apache2;
+use strict;
+use warnings;
+use Apache2::RequestRec;
+use Apache2::RequestIO;
+use Apache2::RequestUtil;
+use Apache2::Response;
+use Apache2::Const -compile => qw(OK);
+use IO::Handle;
+use Plack::Util;
+
+my %apps; # psgi file to $app mapping
+
+sub handler {
+    my $r = shift;
+
+    my $psgi = $r->dir_config('psgi_app');
+    my $app = $apps{$psgi} ||= do {
+        my $app = do $psgi;
+        unless (defined $app && ref $app eq 'CODE') {
+            die "Can't load psgi_app from $psgi";
+        }
+        $app;
+    };
+
+    $r->subprocess_env; # let Apache create %ENV for us :)
+
+    my $env = {
+        %ENV,
+        'psgi.version'        => [ 1, 0 ],
+        'psgi.url_scheme'     => ($ENV{HTTPS}||'off') =~ /^(?:on|1)$/i ? 'https' : 'http',
+        'psgi.input'          => $r,
+        'psgi.errors'         => *STDERR, # xxx
+        'psgi.multithread'    => Plack::Util::FALSE,
+        'psgi.multiprocess'   => Plack::Util::TRUE,
+        'psgi.run_once'       => Plack::Util::FALSE,
+    };
+
+    my $res = $app->($env);
+
+    my $headers = ($res->[0] >= 200 && $res->[0] < 300)
+        ? $r->headers_out : $r->err_headers_out;
+
+    while (my($h, $v) = splice(@{$res->[1]}, 0, 2)) {
+        if ($h =~ /Content-Type/i) {
+            $r->content_type($v);
+        } elsif ($h =~ /Content-Length/i) {
+            $r->set_content_length($v);
+        } else {
+            $headers->add($h => $v);
+        }
+    }
+
+    $r->status($res->[0]);
+    # TODO $r->sendfile support?
+    Plack::Util::foreach($res->[2], sub { $r->puts(@_) });
+
+    return Apache2::Const::OK;
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+Plack::Impl::Apache2 - Apache 2.0 handlers to run PSGI application
+
+=head1 SYNOPSIS
+
+  <Locaion />
+  SetHandler perl-script
+  PerlHandler Plack::Impl::Apache2
+  SetPerlVar psgi_app /path/to/app.psgi
+  </Location>
+
+=head1 AUTHOR
+
+Tatsuhiko Miyagawa
+
+=cut
