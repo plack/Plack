01d67b7cf43fd931958ca808a445aecbfacf0b1c
Kazuho Oku <kazuhooku@gmail.com>
2009-09-18 08:45:52 +0900

    add Server::Starter support

--- lib/Plack/Impl/Standalone.pm
+++ lib/Plack/Impl/Standalone.pm
@@ -18,15 +18,39 @@ our $HasSendFile = do {
     eval { require Sys::Sendfile; 1 };
 };
 
+our $HasServerStarter = do {
+    local $@;
+    eval { require Server::Starter; 1 };
+};
+
 sub new {
     my($class, %args) = @_;
-    bless {
+    my $self = bless {
         host               => $args{host} || 0,
         port               => $args{port} || 8080,
         timeout            => $args{timeout} || 300,
         max_keepalive_reqs => $args{max_keepalive_reqs} || 100,
         keepalive_timeout  => $args{keepalive_timeout} || 5,
     }, $class;
+
+    # setup immediately if there is a superdaemon
+    if ($HasServerStarter && $ENV{SERVER_STARTER_PORT}) {
+        $self->{listen_sock} = IO::Socket::INET->new(
+            Proto => 'tcp',
+        ) or die "failed to create socket:$!";
+        my ($hostport, $fd) = %{Server::Starter::server_ports()};
+        print STDERR "HOSTPORT: $hostport, fd: $fd\n";
+        if ($hostport =~ /(.*):(\d+)/) {
+            $self->{host} = $1;
+            $self->{port} = $2;
+        } else {
+            $self->{port} = $hostport;
+        }
+        $self->{listen_sock}->fdopen($fd, 'w')
+            or die "failed to bind to listening socket:$!";
+    }
+
+    $self;
 }
 
 sub run {
@@ -37,7 +61,7 @@ sub run {
 
 sub setup_listener {
     my $self = shift;
-    $self->{listen_sock} = IO::Socket::INET->new(
+    $self->{listen_sock} ||= IO::Socket::INET->new(
         Listen    => SOMAXCONN,
         LocalPort => $self->{port},
         LocalAddr => $self->{host},
