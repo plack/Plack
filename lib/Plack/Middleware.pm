f63126d2e03af8542c7bd318274f5f7a97664206
yappo <yappo@shibuya.pl>
2009-09-29 20:36:41 +0900

    fixed typo

--- lib/Plack/Middleware.pm
+++ lib/Plack/Middleware.pm
@@ -72,7 +72,7 @@ Plack::Middleware is an utility base class to write PSGI
 middleware. All you have to do is to inherit from Plack::Middleware
 and then implement the callback C<call> method (or C<to_app> method
 that would return the PSGI code reference) to do the actual work. You
-can use C<< $self->app >> to call the original (wrapped) appilcation.
+can use C<< $self->app >> to call the original (wrapped) application.
 
 See L<Plack::Builder> how to actually enable them in your I<.psgi>
 application file using the DSL. If you do not like our builder DSL,
