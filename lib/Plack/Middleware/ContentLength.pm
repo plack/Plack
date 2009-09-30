d449d77fe6398de8541093a920d347752a75f5c3
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2009-09-29 18:23:53 -0700

    docs for ContentLength middleware

--- lib/Plack/Middleware/ContentLength.pm
+++ lib/Plack/Middleware/ContentLength.pm
@@ -21,3 +21,44 @@ sub call {
 }
 
 1;
+
+__END__
+
+=head1 NAME
+
+Plack::Middleware::ContentLength - Adds Content-Length header automatically
+
+=head1 SYNOPSIS
+
+  # in app.psgi
+  use Plack::Middleware qw(ContentLength);
+
+  builder {
+      enable Plack::Middleware::ContentLength;
+      $app;
+  }
+
+  # Or in Plack::Server::*
+  $app = Plack::Middleware::ContentLength->wrap($app);
+
+=head1 DESCRIPTION
+
+Plack::Middleware::ContentLength is a middleware that automatically
+adds C<Content-Length> header when it's appropriate i.e. the response
+has a content body with calculatable size (array of chunks or a real
+filehandle).
+
+This middleware can also be used as a library from PSGI server
+implementations to automatically set C<Content-Length> rather than in
+the end user level.
+
+=head1 AUTHOR
+
+Tatsuhiko Miyagawa
+
+=head1 SEE ALSO
+
+Rack::ContentLength
+
+=cut
+
