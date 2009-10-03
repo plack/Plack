b72150b4e04a32c500b073cfdb2885faa34bc069
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2009-10-02 19:04:56 -0700

    Added Plack::Middleware::ErrorDocument

---
+++ lib/Plack/Middleware/ErrorDocument.pm
@@ -0,0 +1,103 @@
+package Plack::Middleware::ErrorDocument;
+use strict;
+use warnings;
+use base qw(Plack::Middleware);
+__PACKAGE__->mk_accessors(qw(subrequest));
+
+use HTTP::Status qw(is_error);
+
+sub call {
+    my $self = shift;
+    my $env  = shift;
+
+    my $r = $self->app->($env);
+    unless (is_error($r->[0]) && exists $self->{$r->[0]}) {
+        return $r;
+    }
+
+    my $path = $self->{$r->[0]};
+    if ($self->subrequest) {
+        for my $key (keys %$env) {
+            unless ($key =~ /^psgi/) {
+                $env->{'psgix.errordocument.' . $key} = $env->{$key};
+            }
+        }
+
+        # TODO: What if SCRIPT_NAME is not empty?
+        $env->{REQUEST_METHOD} = 'GET';
+        $env->{REQUEST_URI}    = $path;
+        $env->{PATH_INFO}      = $path;
+        $env->{QUERY_STRING}   = '';
+        delete $env->{CONTENT_LENGTH};
+
+        my $sub_r = $self->app->($env);
+        if ($sub_r->[0] == 200) {
+            $r->[1] = $sub_r->[1];
+            $r->[2] = $sub_r->[2];
+        }
+        # TODO: allow 302 here?
+    } else {
+        open my $fh, "<", $path or die "$path: $!";
+        $r->[2] = $fh;
+    }
+
+    return $r;
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+Plack::Middleware::ErrorDocument - Set Error Document based on HTTP status code
+
+=head1 SYNOPSIS
+
+  # in app.psgi
+  use Plack::Middleware qw(ErrorDocument);
+  use Plack::Builder;
+
+  builder {
+      enable Plack::Middleware::ErrorDocument
+          500 => '/uri/errors/500.html', 404 => '/uri/errors/404.html',
+          subrequest => 1;
+      $app;
+  };
+
+=head1 DESCRIPTION
+
+Plack::Middleware::ErrorDocument allows you to customize error screen
+by setting paths (file system path or URI path) of error pages per
+status code.
+
+=head1 CONFIGURATIONS
+
+=over 4
+
+=item subrequest
+
+A boolean flag to serve error pages using a new GET sub request.
+Defaults to false, which means it serves error pages using file
+system path.
+
+  builder {
+      enable Plack::Middleware::ErrorDocument
+          502 => '/home/www/htdocs/errors/maint.html';
+      enable Plack::Middleware::ErrorDocument
+          404 => '/static/404.html', 403 => '/static/403.html', subrequest => 1;
+      $app;
+  };
+
+This configuration serves 502 error pages from file system directly
+assuming that's when you probably maintain database etc. but serves
+404 and 403 pages using a sub request so your application can do some
+logic there like logging or doing suggestions.
+
+=head1 AUTHOR
+
+Tatsuhiko Miyagawa
+
+=head1 SEE ALSO
+
+=cut
---
+++ t/Plack-Middleware/error_document.t
@@ -0,0 +1,42 @@
+use strict;
+use warnings;
+use FindBin;
+use Test::More;
+use HTTP::Request::Common;
+use Plack::Test;
+use Plack::Middleware qw(ErrorDocument Static);
+use Plack::Builder;
+
+my $log;
+my $handler = builder {
+    enable Plack::Middleware::ErrorDocument
+        500 => "$FindBin::Bin/errors/500.html";
+    enable Plack::Middleware::ErrorDocument
+        404 => "/errors/404.html", subrequest => 1;
+    enable Plack::Middleware::Static
+        path => qr{^/errors}, root => $FindBin::Bin;
+
+    sub {
+        my $env = shift;
+        my $status = ($env->{PATH_INFO} =~ m!status/(\d+)!)[0] || 200;
+        [ $status, [ 'Content-Type' => 'text/plain' ], [ "Error: $status" ] ];
+    };
+};
+
+test_psgi app => $handler, client => sub {
+    my $cb = shift;
+    {
+        my $res = $cb->(GET "http://localhost/");
+        is $res->code, 200;
+
+        $res = $cb->(GET "http://localhost/status/500");
+        is $res->code, 500;
+        like $res->content, qr/fancy 500/;
+
+        $res = $cb->(GET "http://localhost/status/404");
+        is $res->code, 404;
+        like $res->content, qr/fancy 404/;
+    }
+};
+
+done_testing;
