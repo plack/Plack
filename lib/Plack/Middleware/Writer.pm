bc75182427e2b2030667e20575e93647871e87ef
Yuval Kogman <nothingmuch@woobling.org>
2009-10-16 01:08:12 +0200

    Add buffering Plack::Middleware::Writer
    Implements return sub { ... } for backends which can't actually stream

---
+++ lib/Plack/Middleware/Writer.pm
@@ -0,0 +1,64 @@
+package Plack::Middleware::Writer;
+use strict;
+no warnings;
+use Carp;
+use Plack::Util;
+use IO::Handle::Util;
+use Scalar::Util qw(weaken);
+use parent qw(Plack::Middleware);
+
+sub call {
+    my ( $self, $env ) = @_;
+
+    my $res = $self->app->($env);
+
+    if ( ref($res) eq 'CODE' ) {
+        my $ret;
+
+        $res->(sub {
+            my $write = shift;
+
+            if ( @$write == 2 ) {
+                my ( $closed, @body );
+
+                $ret = [ @$write, \@body ];
+
+                # two copies because we weaken the one that is closed over
+                my $writer;
+                my $ref_up = $writer = Plack::Util::inline_object(
+                    poll_cb => sub {
+                        my $cb = shift;
+
+                        until ( $closed ) {
+                            $cb->($writer);
+                        }
+                    },
+                    write => sub {
+                        push @body, $_[0];
+                    },
+                    close => sub {
+                        $closed = 1;
+                    }
+                );
+
+                weaken($writer);
+
+                return $writer;
+            } else {
+                $ret = $write;
+                return;
+            }
+        });
+
+        return $ret;
+    } else {
+        return $res;
+    }
+}
+
+
+# ex: set sw=4 et:
+
+__PACKAGE__
+
+__END__
