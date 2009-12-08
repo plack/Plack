18db6b4aab604e195f0035444675e10d70a67382
Stevan Little <stevan.little@iinteractive.com>
2009-12-08 14:35:25 -0500

    moving response_cb up to Middleware since it is not relevant to Plack::App:: modules and therefore to the Plack::Component class

--- lib/Plack/Middleware.pm
+++ lib/Plack/Middleware.pm
@@ -22,6 +22,52 @@ sub wrap {
     return $self->to_app;
 }
 
+sub response_cb {
+    my($self, $res, $cb) = @_;
+
+    my $body_filter = sub {
+        my($cb, $res) = @_;
+        my $filter_cb = $cb->($res);
+        # If response_cb returns a callback, treat it as a $body filter
+        if (defined $filter_cb && ref $filter_cb eq 'CODE') {
+            if (defined $res->[2]) {
+                my $body    = $res->[2];
+                my $getline = ref $body eq 'ARRAY' ? sub { shift @$body } : sub { $body->getline };
+                $res->[2] = Plack::Util::inline_object
+                    getline => sub { $filter_cb->($getline->()) },
+                    close => sub { $body->close if ref $body ne 'ARRAY' };
+            } else {
+                return $filter_cb;
+            }
+        }
+    };
+
+    if (ref $res eq 'ARRAY') {
+        $body_filter->($cb, $res);
+        return $res;
+    } elsif (ref $res eq 'CODE') {
+        return sub {
+            my $respond = shift;
+            $res->(sub {
+                my $res = shift;
+                my $filter_cb = $body_filter->($cb, $res);
+                if ($filter_cb) {
+                    my $writer = $respond->($res);
+                    if ($writer) {
+                        return Plack::Util::inline_object
+                            write => sub { $writer->write($filter_cb->(@_)) },
+                            close => sub { $writer->write($filter_cb->(undef)); $writer->close };
+                    }
+                } else {
+                    return $respond->($res);
+                }
+            });
+        };
+    }
+
+    return $res;
+}
+
 1;
 
 __END__
