8d26f947ec77e8899502114d09ff8d92587f2c40
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2010-02-02 01:28:30 -0800

    Added docs. Rethrow uncaught errors or with code that are not errors.

--- lib/Plack/Middleware/HTTPExceptions.pm
+++ lib/Plack/Middleware/HTTPExceptions.pm
@@ -31,6 +31,10 @@ sub transform_error {
         $code = 500;
     }
 
+    unless ($code >= 300 && $code <= 599) {
+        die "rethrow $e"; # rethrow
+    }
+
     $message ||= HTTP::Status::status_message($code);
 
     return [ $code, [ 'Content-Type' => 'text/plain', 'Content-Length' => length($message) ], [ $message ] ];
@@ -38,4 +42,49 @@ sub transform_error {
 
 1;
 
+__END__
 
+=head1 NAME
+
+Plack::Middleware::HTTPExceptions - Catch HTTP exceptions
+
+=head1 SYNOPSIS
+
+  my $app = sub {
+      # ...
+      MyHTTPError::BadGateway->throw;
+  };
+
+  builder {
+      enable "HTTPExceptions";
+      $app;
+  };
+
+=head1 DESCRIPTION
+
+Plack::Middleware::HTTPExceptions is a PSGI middleware component to
+catch exceptions from applicaitions that can be translated into HTTP
+status code.
+
+Your application is supposed to throw an object that implements
+C<code> method which returns the HTTP status code such as 501 or
+404. This middleware catches them and creates a valid response out of
+the code.
+
+The exception object may also implement C<as_string>, or overload the
+stringification, to represent the text of the error, which defaults to
+the status message of error codes, such as I<Service Unavailable> for
+C<503>.
+
+All the other errors that can't be translated into HTTP errors are
+just rethrown to the outer frame.
+
+=head1 AUTHOR
+
+Tatsuhiko Miyagawa
+
+=head1 SEE ALSO
+
+paste.httpexceptions
+
+=cut
