64f8ab3d6498b65e5432175efeb68f786ae05137
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2010-02-16 20:10:22 -0800

    docs

--- lib/Plack/Middleware/Recursive.pm
+++ lib/Plack/Middleware/Recursive.pm
@@ -85,3 +85,49 @@ sub as_string {
 package Plack::Middleware::Recursive;
 
 1;
+
+__END__
+
+=head1 NAME
+
+Plack::Middleware::Recursive - Allows PSGI apps to include or forward requests recursively
+
+=head1 SYNOPSIS
+
+  # with Builder
+  enable "Recursive";
+
+  # in apps
+  my $res = $env->{'plack.recursive.include'}->("/new_path");
+
+  # Or, use exceptions
+  my $app = sub {
+      # ...
+      Plack::Recursive::ForwardRequest->throw("/new_path");
+  };
+
+=head1 DESCRIPTION
+
+Plack::Middleware::Recursive allows PSGI applications to recursively
+include or forward requests to other paths. Applications can make use
+of callbacks stored in C<< $env->{'plack.recursive.include'} >> to
+I<include> another path to get the response (whether it's an array ref
+or a code ref depending on your application), or throw an exception
+Plack::Recursive::ForwardRequest anywhere in the code to I<forward>
+the current request (i.e. abort the current and redo the request).
+
+=head1 AUTHORS
+
+Tatsuhiko Miyagawa
+
+Masahiro Honma
+
+=head1 SEE ALSO
+
+L<Plack> L<Plack::Middleware::HTTPExceptions>
+
+The idea, code and interface are stolen from Rack::Recursive and paste.recursive.
+
+=cut
+
+
