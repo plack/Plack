9088fae3356d60be18ea8d275b5224f1eb6b30ab
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2010-02-23 01:56:29 -0800

    Checking in changes prior to tagging of version 0.9911.  Changelog diff is:
    diff --git a/Changes b/Changes
    index 50b52f8..5a9a8d5 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,9 @@ Revision history for Perl extension Plack
    
     Take a look at http://github.com/miyagawa/Plack/issues for the planned changes before 1.0 release.
    
    +0.9911  Tue Feb 23 01:55:04 PST 2010
    +        - Removed psgix.io extension to fix streaming choke issue on HTTP::Server::PSGI (tomyhero)
    +
     0.9910  Mon Feb 22 19:03:17 PST 2010
             - This is the first non-dev release since 0.99. Read all the change logs below.
             - Support streaming in JSONP (hiratara)

--- lib/Plack.pm
+++ lib/Plack.pm
@@ -3,7 +3,7 @@ package Plack;
 use strict;
 use warnings;
 use 5.008_001;
-our $VERSION = '0.9910';
+our $VERSION = '0.9911';
 $VERSION = eval $VERSION;
 
 1;
--- lib/Plack/Request.pm
+++ lib/Plack/Request.pm
@@ -2,7 +2,7 @@ package Plack::Request;
 use strict;
 use warnings;
 use 5.008_001;
-our $VERSION = '0.9910';
+our $VERSION = '0.9911';
 $VERSION = eval $VERSION;
 
 use HTTP::Headers;
