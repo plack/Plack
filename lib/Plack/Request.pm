a914ffe080f8ad70add5fde743e6a1da5fe910d2
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2010-03-03 16:06:11 -0800

    Checking in changes prior to tagging of version 0.9914.  Changelog diff is:
    diff --git a/Changes b/Changes
    index 179b8c4..d4737f3 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,12 @@ Revision history for Perl extension Plack
    
     Take a look at http://github.com/miyagawa/Plack/issues for the planned changes before 1.0 release.
    
    +0.9914  Wed Mar  3 16:02:38 PST 2010
    +        - Fixed psgix.io and nested closure for perl 5.8 (hiratara)
    +        - Added an inheritance friendly Apache2 interface (frodwith)
    +        - HTTP::Server::PSGI: Close client connection in the first run (hirose31)
    +        - Fixed Loader/auto.t to reset env var (gugod)
    +
     0.9913  Thu Feb 25 19:14:40 PST 2010
             - Revive psgix.io in HTTP::Server::PSGI (hiratara)
             - Fix packaging issue

--- lib/Plack/Request.pm
+++ lib/Plack/Request.pm
@@ -2,7 +2,7 @@ package Plack::Request;
 use strict;
 use warnings;
 use 5.008_001;
-our $VERSION = '0.9913';
+our $VERSION = '0.9914';
 $VERSION = eval $VERSION;
 
 use HTTP::Headers;
--- lib/Plack/Response.pm
+++ lib/Plack/Response.pm
@@ -1,7 +1,7 @@
 package Plack::Response;
 use strict;
 use warnings;
-our $VERSION = '0.9913';
+our $VERSION = '0.9914';
 $VERSION = eval $VERSION;
 
 use Plack::Util::Accessor qw(body status);
