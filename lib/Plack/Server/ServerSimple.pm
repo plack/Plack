4eb51c707258324656facb7f819bf34869ae3847
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2010-03-08 18:24:28 +0900

    Checking in changes prior to tagging of version 0.9915.  Changelog diff is:
    diff --git a/Changes b/Changes
    index d4737f3..62129b6 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,10 @@ Revision history for Perl extension Plack
    
     Take a look at http://github.com/miyagawa/Plack/issues for the planned changes before 1.0 release.
    
    +0.9915  Mon Mar  8 18:22:33 JST 2010
    +        - Fixed a dumb bug in Plack::Handler::Apache2, broken in 0.9914 (hiratara)
    +        - Added a warning if you misuse mount()
    +
     0.9914  Wed Mar  3 16:02:38 PST 2010
             - Fixed psgix.io and nested closure for perl 5.8 (hiratara)
             - Added an inheritance friendly Apache2 interface (frodwith)

--- lib/Plack/Server/ServerSimple.pm
+++ lib/Plack/Server/ServerSimple.pm
@@ -1,6 +1,6 @@
 package Plack::Server::ServerSimple;
 use strict;
-our $VERSION = '0.9914';
+our $VERSION = '0.9915';
 $VERSION = eval $VERSION;
 
 use parent qw(Plack::Handler::HTTP::Server::Simple);
