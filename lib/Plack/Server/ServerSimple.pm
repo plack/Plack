19a80b2906491e44c49fc5d2f0a1e7f8630e5769
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2010-03-12 12:55:29 +0900

    Checking in changes prior to tagging of version 0.9916.  Changelog diff is:
    diff --git a/Changes b/Changes
    index 62129b6..0e98fda 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,12 @@ Revision history for Perl extension Plack
    
     Take a look at http://github.com/miyagawa/Plack/issues for the planned changes before 1.0 release.
    
    +0.9916  Fri Mar 12 12:52:39 JST 2010
    +        - Added support for a new (renamed) web server Corona
    +        - Document enable coderef in Plack::Middleware (clkao)
    +        - Middleware::StackTrace: Send plain text errors to clients that probably do
    +          not understand HTML like curl
    +
     0.9915  Mon Mar  8 18:22:33 JST 2010
             - Fixed a dumb bug in Plack::Handler::Apache2, broken in 0.9914 (hiratara)
             - Added a warning if you misuse mount()

--- lib/Plack/Server/ServerSimple.pm
+++ lib/Plack/Server/ServerSimple.pm
@@ -1,6 +1,6 @@
 package Plack::Server::ServerSimple;
 use strict;
-our $VERSION = '0.9915';
+our $VERSION = '0.9916';
 $VERSION = eval $VERSION;
 
 use parent qw(Plack::Handler::HTTP::Server::Simple);
