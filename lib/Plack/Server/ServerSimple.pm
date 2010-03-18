f716b30889da148708126663bec4134fab0ef60b
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2010-03-17 22:38:26 -0700

    Checking in changes prior to tagging of version 0.9918.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 24ac25e..2069a9a 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,9 @@ Revision history for Perl extension Plack
    
     Take a look at http://github.com/miyagawa/Plack/issues for the planned changes before 1.0 release.
    
    +0.9918  Wed Mar 17 22:35:00 PDT 2010
    +        - Load .psgi file in a sandboxed package rather than Plack::Util gh-88
    +
     0.9917  Wed Mar 17 15:33:43 PDT 2010
             - Added Plack::Handler::Apache2::Registry (hiratara)
             - Set default PLACK_ENV in Plack::Util::load_psgi

--- lib/Plack/Server/ServerSimple.pm
+++ lib/Plack/Server/ServerSimple.pm
@@ -1,6 +1,6 @@
 package Plack::Server::ServerSimple;
 use strict;
-our $VERSION = '0.9917';
+our $VERSION = '0.9918';
 $VERSION = eval $VERSION;
 
 use parent qw(Plack::Handler::HTTP::Server::Simple);
