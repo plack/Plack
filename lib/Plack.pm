7b557de40f01ca80c4715f0dfd0ff3bde14d9258
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2010-03-25 15:08:27 -0700

    Checking in changes prior to tagging of version 0.99_21.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index d6591ac..b0bc988 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,12 @@ Revision history for Perl extension Plack
    
     Take a look at http://github.com/miyagawa/Plack/issues for the planned changes before 1.0 release.
    
    +0.99_21 Thu Mar 25 15:05:53 PDT 2010
    +        - INCOMPATIBLE: rmoved a workaround for lighttpd SCRIPT_NAME bug in FCGI handler
    +          See http://github.com/miyagawa/Plack/issues#issue/68 for details.
    +        - HTTPException now logs standard exceptions to psgi.errors
    +        - micro optimization for Plack::Request content() method
    +
     0.9920  Thu Mar 18 23:48:06 PDT 2010
             - Fixed URL path prefix matching in URLMap (hiratara)
             - Fixed Plack::Request->content on GET with FastCGI servers (sunnavy)

--- lib/Plack.pm
+++ lib/Plack.pm
@@ -3,7 +3,7 @@ package Plack;
 use strict;
 use warnings;
 use 5.008_001;
-our $VERSION = '0.9920';
+our $VERSION = '0.99_21';
 $VERSION = eval $VERSION;
 
 1;
--- lib/Plack/Request.pm
+++ lib/Plack/Request.pm
@@ -2,7 +2,7 @@ package Plack::Request;
 use strict;
 use warnings;
 use 5.008_001;
-our $VERSION = '0.9920';
+our $VERSION = '0.99_21';
 $VERSION = eval $VERSION;
 
 use HTTP::Headers;
