3b9b567bcde648a4f86c560c76e2448d30ad48f5
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2010-03-27 01:04:22 -0700

    Checking in changes prior to tagging of version 0.99_23.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 0664f49..bef2302 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,10 @@ Revision history for Perl extension Plack
    
     Take a look at http://github.com/miyagawa/Plack/issues for the planned changes before 1.0 release.
    
    +0.99_23 Sat Mar 27 01:02:24 PDT 2010
    +        - Dropped keep-alive code from HTTP::Server::PSGI now that Starlet clones the code
    +        - Special case --disable-* and --enable-* command line options in plackup and Plack::Runner
    +
     0.99_22 Thu Mar 25 19:48:08 PDT 2010
             - INCOMPATIBLE: removed --max-workers option from the default standalone server.
               Now it gives you warnings and falls back to the single process mode.

--- lib/Plack.pm
+++ lib/Plack.pm
@@ -3,7 +3,7 @@ package Plack;
 use strict;
 use warnings;
 use 5.008_001;
-our $VERSION = '0.99_22';
+our $VERSION = '0.99_23';
 $VERSION = eval $VERSION;
 
 1;
--- lib/Plack/Server/ServerSimple.pm
+++ lib/Plack/Server/ServerSimple.pm
@@ -1,6 +1,6 @@
 package Plack::Server::ServerSimple;
 use strict;
-our $VERSION = '0.99_22';
+our $VERSION = '0.99_23';
 $VERSION = eval $VERSION;
 
 use parent qw(Plack::Handler::HTTP::Server::Simple);
