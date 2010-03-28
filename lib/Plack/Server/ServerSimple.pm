6f8623a819c309e63cdc8a5702c39c2507d22aae
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2010-03-28 14:48:40 -0700

    Checking in changes prior to tagging of version 0.9926.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 9d2db00..041f3be 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,9 @@ Revision history for Perl extension Plack
    
     Take a look at http://github.com/miyagawa/Plack/issues for the planned changes before 1.0 release.
    
    +0.9926 Sun Mar 28 14:37:03 PDT 2010
    +        - Added -v|--version option to plackup and the ability for Plack::Runner users to override
    +
     0.9925  Sat Mar 27 19:03:57 PDT 2010
             - Make this a non-devel release

--- lib/Plack/Server/ServerSimple.pm
+++ lib/Plack/Server/ServerSimple.pm
@@ -1,6 +1,6 @@
 package Plack::Server::ServerSimple;
 use strict;
-our $VERSION = '0.9925';
+our $VERSION = '0.9926';
 $VERSION = eval $VERSION;
 
 use parent qw(Plack::Handler::HTTP::Server::Simple);
