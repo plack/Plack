58361b913be81e0799d8de7a936e7941d1794697
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2010-03-30 16:26:55 -0700

    lighttpd < 1.4.17 has bugs that adds extra Content-Length: 0 in 304 responses. http://www.cpantesters.org/cpan/report/7040400

--- t/FCGIUtils.pm
+++ t/FCGIUtils.pm
@@ -54,6 +54,10 @@ sub test_lighty_external (&@) {
         unless $lighttpd_bin && -x $lighttpd_bin;
 
     my $ver = (`$lighttpd_bin -v` =~ m!lighttpd[-/]1.(\d+\.\d+)!)[0];
+    if ($ver < 4.17) {
+        plan skip_all => "Too old lighttpd (1.$ver), known to be broken";
+    }
+
     diag "Testing with lighttpd 1.$ver";
 
     my $tmpdir = File::Temp::tempdir( CLEANUP => 1 );
