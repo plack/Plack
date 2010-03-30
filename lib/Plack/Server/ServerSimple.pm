826b72c5f49d52f1f9bb38324ab696f69c93565a
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2010-03-29 17:03:52 -0700

    Checking in changes prior to tagging of version 0.9928.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index f00e3ea..4bd0363 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,9 @@ Revision history for Perl extension Plack
    
     Take a look at http://github.com/miyagawa/Plack/issues for the planned changes before 1.0 release.
    
    +0.9928  Mon Mar 29 17:02:42 PDT 2010
    +        - log_dispatch.t: require Log::Dispatch::Array
    +
     0.9927  Mon Mar 29 12:43:44 PDT 2010
             - require newer Log::Dispatch (confound)
             - StackTrace: Encode exceptions in utf-8 in case they include wide characters #95 (tokuhirom)

--- lib/Plack/Server/ServerSimple.pm
+++ lib/Plack/Server/ServerSimple.pm
@@ -1,6 +1,6 @@
 package Plack::Server::ServerSimple;
 use strict;
-our $VERSION = '0.9927';
+our $VERSION = '0.9928';
 $VERSION = eval $VERSION;
 
 use parent qw(Plack::Handler::HTTP::Server::Simple);
