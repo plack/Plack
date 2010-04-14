a0516a620352ec463b2fdb0157f1f281f0ea3fa9
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2010-04-13 20:21:50 -0700

    Checking in changes prior to tagging of version 0.9930.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 0f0ab40..341971f 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,12 @@ Revision history for Perl extension Plack
    
     Take a look at http://github.com/miyagawa/Plack/issues for the planned changes before 1.0 release.
    
    +0.9930  Tue Apr 13 20:18:06 PDT 2010
    +        - Added Plack::Handler::Net::FastCGI (chansen)
    +        - Made Test::TCP a hard dependency since Plack::Test needs it
    +        - Added Delayed loader for Starlet and Starman (clkao)
    +        - Hide logger middleware from log4perl's caller stack (haarg)
    +
     0.9929  Wed Mar 31 00:33:10 PDT 2010
             - Middleware::JSONP: Simplified code and does not support IO response body type
             - fcgi.t: skip tests with lighttpd < 1.4.17 per CPAN Testers #7040400

--- lib/Plack.pm
+++ lib/Plack.pm
@@ -3,7 +3,7 @@ package Plack;
 use strict;
 use warnings;
 use 5.008_001;
-our $VERSION = '0.9929';
+our $VERSION = '0.9930';
 $VERSION = eval $VERSION;
 
 1;
--- lib/Plack/Server/ServerSimple.pm
+++ lib/Plack/Server/ServerSimple.pm
@@ -1,6 +1,6 @@
 package Plack::Server::ServerSimple;
 use strict;
-our $VERSION = '0.9929';
+our $VERSION = '0.9930';
 $VERSION = eval $VERSION;
 
 use parent qw(Plack::Handler::HTTP::Server::Simple);
