bcd14bfbfd4446e77cd0ffea5f91c651d09c6611
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2010-05-14 16:00:24 -0700

    Checking in changes prior to tagging of version 0.9936.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 2335060..4ab483e 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,11 @@ Revision history for Perl extension Plack
    
     Take a look at http://github.com/miyagawa/Plack/issues for the planned changes before 1.0 release.
    
    +0.9936  Fri May 14 15:58:02 PDT 2010
    +        - Remove 'use lib "lib"' from plackup
    +        - Remove HTTP_CONTENT_* environment variables in FastCGI handlers to deal with buggy web servers.
    +          (Justin Davis)
    +
     0.9935  Wed May  5 15:17:06 PDT 2010
             - Set an empty PATH_INFO if CGI environment doesn't set so (hachi) #109
             - Fixed a possible weird combination of SCRIPT_NAME and PATH_INFO in CGI handlers

--- lib/Plack/Request.pm
+++ lib/Plack/Request.pm
@@ -2,7 +2,7 @@ package Plack::Request;
 use strict;
 use warnings;
 use 5.008_001;
-our $VERSION = '0.9935';
+our $VERSION = '0.9936';
 $VERSION = eval $VERSION;
 
 use HTTP::Headers;
--- lib/Plack/Server/ServerSimple.pm
+++ lib/Plack/Server/ServerSimple.pm
@@ -1,6 +1,6 @@
 package Plack::Server::ServerSimple;
 use strict;
-our $VERSION = '0.9935';
+our $VERSION = '0.9936';
 $VERSION = eval $VERSION;
 
 use parent qw(Plack::Handler::HTTP::Server::Simple);
