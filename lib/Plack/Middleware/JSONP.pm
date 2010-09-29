1251d517bafe181b2e901cbd7df164043040659b
franck cuny <franck@lumberjaph.net>
2010-09-29 11:25:59 +0200

    support callback params other than 'callback' for JSONP middleware

--- lib/Plack/Middleware/JSONP.pm
+++ lib/Plack/Middleware/JSONP.pm
@@ -4,6 +4,8 @@ use parent qw(Plack::Middleware);
 use Plack::Util;
 use URI::Escape ();
 
+use Plack::Util::Accessor qw/callback_key/;
+
 sub call {
     my($self, $env) = @_;
     my $res = $self->app->($env);
@@ -11,9 +13,9 @@ sub call {
         my $res = shift;
         if (defined $res->[2] && ref $res->[2] eq 'ARRAY' && @{$res->[2]} == 1) {
             my $h = Plack::Util::headers($res->[1]);
+            my $callback_key = $self->callback_key || 'callback';
             if ($h->get('Content-Type') =~ m!/(?:json|javascript)! &&
-                $env->{QUERY_STRING} =~ /(?:^|&)callback=([^&]+)/) {
-                # TODO: support callback params other than 'callback'
+                $env->{QUERY_STRING} =~ /(?:^|&)$callback_key=([^&]+)/) {
                 my $cb = URI::Escape::uri_unescape($1);
                 if ($cb =~ /^[\w\.\[\]]+$/) {
                     my $jsonp = "$cb($res->[2][0])";
@@ -34,11 +36,16 @@ __END__
 
 Plack::Middleware::JSONP - Wraps JSON response in JSONP if callback parameter is specified
 
+=head1 SYNOPSIS
+
+    enable "JSONP", callback_key => 'jsonp';
+
 =head1 DESCRIPTION
 
 Plack::Middleware::JSONP wraps JSON response, which has Content-Type
 value either C<text/javascript> or C<application/json> as a JSONP
-response which is specified with the C<callback> query parameter.
+response which is specified with the C<callback> query parameter. The 
+name of the parameter can be set while enabling the middleware.
 
 This middleware only works with an application response with content
 body set as a single element array ref and doesn't touch the response
