f8be2a82ae39b3fd480fe54e9792eff41bddc945
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2010-12-03 14:52:31 -0800

    Checking in changes prior to tagging of version 0.9953.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index c9773d3..c67bf27 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,11 @@ Revision history for Perl extension Plack
    
     Take a look at http://github.com/miyagawa/Plack/issues for the planned changes before 1.0 release.
    
    +0.9953  Fri Dec  3 14:50:09 PST 2010
    +        - Include the original error message in the StackTrace text output on console.
    +          This requires Devel::StackTrace 1.23 and Devel::StackTrace::WithLexicals 0.08 (optional)
    +        - Fixed AccessLog middleware to handle multiple dashes in %{} (Jiro)
    +
     0.9952  Thu Dec  2 14:03:48 PST 2010
             - Fixed the potential deadlocks in WrapCGI's read/write pipe (typester)
             - Improved documentations on plackup -e

--- lib/Plack/Server/ServerSimple.pm
+++ lib/Plack/Server/ServerSimple.pm
@@ -1,6 +1,6 @@
 package Plack::Server::ServerSimple;
 use strict;
-our $VERSION = '0.9952';
+our $VERSION = '0.9953';
 $VERSION = eval $VERSION;
 
 use parent qw(Plack::Handler::HTTP::Server::Simple);
