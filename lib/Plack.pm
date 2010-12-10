4448aef7f535bfa093b52075752a79ed67138d8b
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2010-12-09 17:47:35 -0800

    Checking in changes prior to tagging of version 0.9954.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 45dbdf0..c756bd2 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,9 @@ Revision history for Perl extension Plack
    
     Take a look at http://github.com/miyagawa/Plack/issues for the planned changes before 1.0 release.
    
    +0.9954  Thu Dec  9 17:45:59 PST 2010
    +        - Fixed a directory traversal bug in Plack::App::File etc. RT:63020
    +
     0.9953  Fri Dec  3 14:50:09 PST 2010
             - Include the original error message in the StackTrace text output on console.
               This requires Devel::StackTrace 1.23 and Devel::StackTrace::WithLexicals 0.08 (optional)

--- lib/Plack.pm
+++ lib/Plack.pm
@@ -3,7 +3,7 @@ package Plack;
 use strict;
 use warnings;
 use 5.008_001;
-our $VERSION = '0.9953';
+our $VERSION = '0.9954';
 $VERSION = eval $VERSION;
 
 1;
--- lib/Plack/Server/ServerSimple.pm
+++ lib/Plack/Server/ServerSimple.pm
@@ -1,6 +1,6 @@
 package Plack::Server::ServerSimple;
 use strict;
-our $VERSION = '0.9953';
+our $VERSION = '0.9954';
 $VERSION = eval $VERSION;
 
 use parent qw(Plack::Handler::HTTP::Server::Simple);
