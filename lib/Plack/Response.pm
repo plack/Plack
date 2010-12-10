1fbc04e3272fcdf02bcac13e2c6f4e7b822cdc52
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2010-12-09 18:03:53 -0800

    Checking in changes prior to tagging of version 0.9955.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index c756bd2..78fefe0 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,9 @@ Revision history for Perl extension Plack
    
     Take a look at http://github.com/miyagawa/Plack/issues for the planned changes before 1.0 release.
    
    +0.9955  Thu Dec  9 18:02:50 PST 2010
    +        - More fixes to a possible directory traversal
    +
     0.9954  Thu Dec  9 17:45:59 PST 2010
             - Fixed a directory traversal bug in Plack::App::File etc. RT:63020

--- lib/Plack/Response.pm
+++ lib/Plack/Response.pm
@@ -1,7 +1,7 @@
 package Plack::Response;
 use strict;
 use warnings;
-our $VERSION = '0.9954';
+our $VERSION = '0.9955';
 $VERSION = eval $VERSION;
 
 use Plack::Util::Accessor qw(body status);
--- lib/Plack/Server/ServerSimple.pm
+++ lib/Plack/Server/ServerSimple.pm
@@ -1,6 +1,6 @@
 package Plack::Server::ServerSimple;
 use strict;
-our $VERSION = '0.9954';
+our $VERSION = '0.9955';
 $VERSION = eval $VERSION;
 
 use parent qw(Plack::Handler::HTTP::Server::Simple);
