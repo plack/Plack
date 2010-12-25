84b0ba5759734330e0af6b733e5ff94cba6c6939
Mark Stosberg <mark@stosberg.com>
2010-12-25 12:41:08 -0500

    Clarify that unlike CGI.pm, this param method is not read/write.
    There is a disclaimer at the top that most methods are read-only, but
    it seems important to re-clarify here, as it affects the claim that
    this method is CGI.pm compatible.

--- lib/Plack/Request.pm
+++ lib/Plack/Request.pm
@@ -542,7 +542,8 @@ Shortcut to $req->headers->user_agent.
 
 Returns GET and POST parameters with a CGI.pm-compatible param
 method. This is an alternative method for accessing parameters in
-$req->parameters.
+$req->parameters. Unlike CGI.pm, it does I<not> allow
+setting or modifying query parameters.
 
     $value  = $req->param( 'foo' );
     @values = $req->param( 'foo' );
