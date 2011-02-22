0725390409264a4779ce0c44ccb61d864ccfe183
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2011-02-22 08:42:26 -0800

    Checking in changes prior to tagging of version 0.9970.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 11607cb..4ccf4fc 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,10 @@ Revision history for Perl extension Plack
    
     Take a look at http://github.com/miyagawa/Plack/issues for the planned changes before 1.0 release.
    
    +0.9970  Tue Feb 22 08:35:50 PST 2011
    +        - Apache2: Fixed a bug where dispatcher fails to parse first path when it begins with two or
    +          more slashes (clkao)
    +
     0.9969  Fri Feb 18 21:35:29 PST 2011
             - Suppress the use of unlocalized $_ in Plack::Runner (mst)
             - Plack::Handler::Net::FastCGI is now removed from Plack core dist.

--- lib/Plack.pm
+++ lib/Plack.pm
@@ -3,7 +3,7 @@ package Plack;
 use strict;
 use warnings;
 use 5.008_001;
-our $VERSION = '0.9969';
+our $VERSION = '0.9970';
 $VERSION = eval $VERSION;
 
 1;
--- lib/Plack/Request.pm
+++ lib/Plack/Request.pm
@@ -2,7 +2,7 @@ package Plack::Request;
 use strict;
 use warnings;
 use 5.008_001;
-our $VERSION = '0.9969';
+our $VERSION = '0.9970';
 $VERSION = eval $VERSION;
 
 use HTTP::Headers;
