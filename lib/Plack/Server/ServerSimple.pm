4ca00bb7519c5b6427834c54ad58f0b5fffa5e52
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2012-03-12 11:29:20 -0700

    Checking in changes prior to tagging of version 0.9986.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 5cd7b63..ef951da 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,12 @@ Revision history for Perl extension Plack
    
     Take a look at http://github.com/miyagawa/Plack/issues for the planned changes before 1.0 release.
    
    +0.9986  Mon Mar 12 11:26:59 PDT 2012
    +    [IMPROVEMENTS]
    +        - Use I/O handles to FCGI::Request instead of global STDIN, STDOUT etc. (chansen)
    +        - Improved FastCGI docs (osfameron)
    +        - Cascade app now returns the last response code (aristotle)
    +
     0.9985  Mon Oct 31 13:11:19 PDT 2011
         [BUG FIXES]
             - Short circuit Plack::Handler fallback to avoid %INC bugs in perl 5.8 (mst)

--- lib/Plack/Server/ServerSimple.pm
+++ lib/Plack/Server/ServerSimple.pm
@@ -1,6 +1,6 @@
 package Plack::Server::ServerSimple;
 use strict;
-our $VERSION = '0.9985';
+our $VERSION = '0.9986';
 $VERSION = eval $VERSION;
 
 use parent qw(Plack::Handler::HTTP::Server::Simple);
