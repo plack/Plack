4d065aa744c8e1e1b38f9f9d428b23240ad49391
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2012-05-11 12:26:13 +0200

    Checking in changes prior to tagging of version 0.9988.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index e910a90..f87158c 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,10 @@ Revision history for Perl extension Plack
    
     Take a look at http://github.com/miyagawa/Plack/issues for the planned changes before 1.0 release.
    
    +0.9988  Fri May 11 12:25:09 CEST 2012
    +    [BUG FIXES]
    +        - Fixes HTTP_HOST in HTTP::Message::PSGI #287 (doy)
    +
     0.9987  Thu May 10 07:06:32 CEST 2012
         [IMPROVEMENTS]
             - Support streaming in AccessLog::Timed (Peter Makholm)

--- lib/Plack/Server/ServerSimple.pm
+++ lib/Plack/Server/ServerSimple.pm
@@ -1,6 +1,6 @@
 package Plack::Server::ServerSimple;
 use strict;
-our $VERSION = '0.9987';
+our $VERSION = '0.9988';
 $VERSION = eval $VERSION;
 
 use parent qw(Plack::Handler::HTTP::Server::Simple);
