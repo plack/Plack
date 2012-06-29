84f6225fb58176d0b2f5c0cd637edb08ef17b6e9
Ricky Morse <remorse@partners.org>
2012-06-29 11:21:50 -0400

    issue 301; support for max-age in cookies

--- lib/Plack/Response.pm
+++ lib/Plack/Response.pm
@@ -138,6 +138,7 @@ sub _bake_cookie {
     push @cookie, "domain=" . $val->{domain}   if $val->{domain};
     push @cookie, "path=" . $val->{path}       if $val->{path};
     push @cookie, "expires=" . $self->_date($val->{expires}) if $val->{expires};
+    push @cookie, "max-age=" . $val->{"max-age"} if $val->{"max-age"};
     push @cookie, "secure"                     if $val->{secure};
     push @cookie, "HttpOnly"                   if $val->{httponly};
 
