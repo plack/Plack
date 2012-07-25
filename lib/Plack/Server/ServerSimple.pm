b8d90c1753e99017947471e6541e52896bdc9da1
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2012-07-25 14:25:04 -0700

    Upped to 1.0000

--- lib/Plack/Server/ServerSimple.pm
+++ lib/Plack/Server/ServerSimple.pm
@@ -1,6 +1,6 @@
 package Plack::Server::ServerSimple;
 use strict;
-our $VERSION = '0.9990';
+our $VERSION = '1.0000';
 $VERSION = eval $VERSION;
 
 use parent qw(Plack::Handler::HTTP::Server::Simple);
