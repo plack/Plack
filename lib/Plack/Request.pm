ea7c0cf18a9f2ab6ef452d5f20a8cf30d06153a8
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2012-07-26 16:27:28 -0700

    Checking in changes prior to tagging of version 1.0001.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 329269d..fa283d6 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,16 @@
     Go to http://github.com/plack/Plack/issues for the roadmap and known issues.
    
    +1.0001  Thu Jul 26 16:24:13 PDT 2012
    +    [INCOMPATIBLE CHANGES]
    +        - Deleted lots of code, methods and warnings that have been deprecated since 0.99
    +          (which should've been done in 1.0000 release)
    +
    +    [DEVELOPERS]
    +        - Added bootstrap script to install devel dependencies
    +
    +    [IMPROVEMENTS]
    +        - Fixed version numbers in some of the modules that have their own $VERSION
    +
     1.0000  Thu Jul 19 18:59:18 PDT 2012
             - This be 1.0! (Same as 0.9991)

--- lib/Plack/Request.pm
+++ lib/Plack/Request.pm
@@ -2,7 +2,7 @@ package Plack::Request;
 use strict;
 use warnings;
 use 5.008_001;
-our $VERSION = '1.0000';
+our $VERSION = '1.0001';
 $VERSION = eval $VERSION;
 
 use HTTP::Headers;
--- lib/Plack/Response.pm
+++ lib/Plack/Response.pm
@@ -1,7 +1,7 @@
 package Plack::Response;
 use strict;
 use warnings;
-our $VERSION = '1.0000';
+our $VERSION = '1.0001';
 $VERSION = eval $VERSION;
 
 use Plack::Util::Accessor qw(body status);
