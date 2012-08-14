53f0f908d20a7830350272b3bb2b47bdf750c9e9
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2012-08-13 17:08:54 -0700

    Checking in changes prior to tagging of version 1.0002.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 799d98c..d1144bf 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,18 @@
     Go to http://github.com/plack/Plack/issues for the roadmap and known issues.
    
    +1.0002  Mon Aug 13 17:04:25 PDT 2012
    +    [NEW FEATURES]
    +        - Added --no-default-middleware option to plackup #290
    +
    +    [BUG FIXES]
    +        - Use C locale for AccessLog strftime #313
    +        - Escape Plack::Request URI path using RFC 3986 definition (ssmccoy)
    +
    +    [IMPROVEMENTS]
    +        - Documentation improvements (ether, Tom Heady)
    +        - Skip displaying ".." in Plack::App::Directory #277
    +        - Document load_class() doesn't validate user input. #285
    +
     1.0001  Thu Jul 26 16:24:13 PDT 2012
         [INCOMPATIBLE CHANGES]
             - Deleted lots of code, methods and warnings that have been deprecated since 0.99

--- lib/Plack.pm
+++ lib/Plack.pm
@@ -3,7 +3,7 @@ package Plack;
 use strict;
 use warnings;
 use 5.008_001;
-our $VERSION = '1.0001';
+our $VERSION = '1.0002';
 $VERSION = eval $VERSION;
 
 1;
--- lib/Plack/Response.pm
+++ lib/Plack/Response.pm
@@ -1,7 +1,7 @@
 package Plack::Response;
 use strict;
 use warnings;
-our $VERSION = '1.0001';
+our $VERSION = '1.0002';
 $VERSION = eval $VERSION;
 
 use Plack::Util::Accessor qw(body status);
