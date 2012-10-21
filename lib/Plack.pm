3958aa4e2d208115425b78e7dcd7d8f73ed7dd15
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2012-10-20 23:22:38 -0700

    Checking in changes prior to tagging of version 1.0007.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 13330ff..76d3cd1 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,10 @@
     Go to http://github.com/plack/Plack/issues for the roadmap and known issues.
    
    +1.0007  Sat Oct 20 23:20:20 PDT 2012
    +    [IMPROVEMENTS]
    +        - Fix test failures with HTTP::Message 6.06. #345
    +        - relaxed plackup -R ignore files and directoris. #260
    +
     1.0006  Thu Oct 18 16:06:15 PDT 2012
         [INCOMPATIBLE CHANGES]
             - plackup foo.psgi will not search the file in @INC anymore before the current directory

--- lib/Plack.pm
+++ lib/Plack.pm
@@ -3,7 +3,7 @@ package Plack;
 use strict;
 use warnings;
 use 5.008_001;
-our $VERSION = '1.0006';
+our $VERSION = '1.0007';
 $VERSION = eval $VERSION;
 
 1;
--- lib/Plack/Response.pm
+++ lib/Plack/Response.pm
@@ -1,7 +1,7 @@
 package Plack::Response;
 use strict;
 use warnings;
-our $VERSION = '1.0006';
+our $VERSION = '1.0007';
 $VERSION = eval $VERSION;
 
 use Plack::Util::Accessor qw(body status);
