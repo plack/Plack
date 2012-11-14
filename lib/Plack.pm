250943e7474ff6e3a11cb7babdfc3b6cbf2cb177
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2012-11-14 12:01:44 -0800

    Checking in changes prior to tagging of version 1.0012.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index fbacef3..835b235 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,10 @@
     Go to http://github.com/plack/Plack/issues for the roadmap and known issues.
    
    +1.0012  Wed Nov 14 12:00:17 PST 2012
    +    [IMPROVEMENTS]
    +        - Make conditional middleware work with initialization without an app (doy)
    +        - Added force option to BufferedStreaming
    +
     1.0011  Sun Nov 11 11:05:30 PST 2012
         [BUG FIXES]
             - Fix bad Content-Length that could be caused with mod_perl (avar)

--- lib/Plack.pm
+++ lib/Plack.pm
@@ -3,7 +3,7 @@ package Plack;
 use strict;
 use warnings;
 use 5.008_001;
-our $VERSION = '1.0011';
+our $VERSION = '1.0012';
 $VERSION = eval $VERSION;
 
 1;
--- lib/Plack/Request.pm
+++ lib/Plack/Request.pm
@@ -2,7 +2,7 @@ package Plack::Request;
 use strict;
 use warnings;
 use 5.008_001;
-our $VERSION = '1.0011';
+our $VERSION = '1.0012';
 $VERSION = eval $VERSION;
 
 use HTTP::Headers;
