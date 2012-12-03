6493698a87bd69f3a80a005dc7285d40f750283f
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2012-12-03 10:29:13 -0800

    Checking in changes prior to tagging of version 1.0014.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 0ffe3da..5f9f549 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,10 @@
     Go to http://github.com/plack/Plack/issues for the roadmap and known issues.
    
    +1.0014 Mon Dec  3 10:27:43 PST 2012
    +    [BUG FIXES]
    +        - Fixed Hash order in tests for perl 5.17 (doy)
    +        - Fixed StackTrace tests to run with Devel::StackTrace
    +
         [IMPROVEMENTS]
             - Plack::Middleware::AccessLog can now log the worker pid and server
               port (ether)

--- lib/Plack.pm
+++ lib/Plack.pm
@@ -3,7 +3,7 @@ package Plack;
 use strict;
 use warnings;
 use 5.008_001;
-our $VERSION = '1.0013';
+our $VERSION = '1.0014';
 $VERSION = eval $VERSION;
 
 1;
--- lib/Plack/Response.pm
+++ lib/Plack/Response.pm
@@ -1,7 +1,7 @@
 package Plack::Response;
 use strict;
 use warnings;
-our $VERSION = '1.0013';
+our $VERSION = '1.0014';
 $VERSION = eval $VERSION;
 
 use Plack::Util::Accessor qw(body status);
