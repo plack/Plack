fc42eafd5748db34f346ba921e272aa3932c4282
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2013-01-10 15:22:06 -0800

    Checking in changes prior to tagging of version 1.0015.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 6d943a0..3575293 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,9 @@
     Go to http://github.com/plack/Plack/issues for the roadmap and known issues.
    
    +1.0015 Thu Jan 10 15:19:17 PST 2013
    +    [BUG FIXES]
    +        - Fixed Lint complaining about Latin-1 range characters stored internally with
    +          utf8 flag on (Mark Fowler)
             - HTTP::Message::PSGI::res_from_psgi now always returns empty string
               for an empty response body, so streamed responses are consistent with
               non-streamed (ether)

--- lib/Plack.pm
+++ lib/Plack.pm
@@ -3,7 +3,7 @@ package Plack;
 use strict;
 use warnings;
 use 5.008_001;
-our $VERSION = '1.0014';
+our $VERSION = '1.0015';
 $VERSION = eval $VERSION;
 
 1;
--- lib/Plack/Request.pm
+++ lib/Plack/Request.pm
@@ -2,7 +2,7 @@ package Plack::Request;
 use strict;
 use warnings;
 use 5.008_001;
-our $VERSION = '1.0014';
+our $VERSION = '1.0015';
 $VERSION = eval $VERSION;
 
 use HTTP::Headers;
