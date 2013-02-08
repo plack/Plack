0759abec7e0ce006e9e104d00ece1ea5f0d9c161
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2013-02-08 11:06:12 -0800

    Added Plack::Util::convert_to_app($app) using more generic coderef overloading
    Now it warns the auto-conversion in development environment for all
    objects that does &{} overloading, not just Plack::Component derived
    classes. The check is done now in two places: Plack::Runner's build
    method, in addition to Plack::Builder's builder { } block.

--- lib/Plack/Builder.pm
+++ lib/Plack/Builder.pm
@@ -124,7 +124,7 @@ sub builder(&) {
         }
     }
 
-    $app = $app->to_app if $app and Scalar::Util::blessed($app) and $app->can('to_app');
+    $app = Plack::Util::convert_to_app($app);
 
     $self->to_app($app);
 }
