d81ca205dc703a19802b1d6a7f4e7a1c571c0380
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2013-02-07 19:43:11 -0800

    Checking in changes prior to tagging of version 1.0017.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 084f1fd..db54eea 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,15 @@
     Go to http://github.com/plack/Plack/issues for the roadmap and known issues.
    
    +1.0017 Thu Feb  7 19:21:24 PST 2013
    +    [INCOMPATIBLE CHANGES]
    +        - Gives you warnings when you use one of Plack::App objects in `plackup -e` or
    +          in .psgi files but forgot to call ->to_app to make it a PSGI application (#369)
    +          Still automatically converts them for backward compatibility, but in the
    +          loading time inside Plack::Builder.
    +
    +    [BUG FIXES]
    +        - chdir to the CGI path when executing CGIBin (#338, #368)
    +
     1.0016 Thu Jan 31 13:21:14 PST 2013
         [SECURITY]
             - Fixed directory traversal bug in Plack::App::File on win32 environments

--- lib/Plack/Request.pm
+++ lib/Plack/Request.pm
@@ -2,7 +2,7 @@ package Plack::Request;
 use strict;
 use warnings;
 use 5.008_001;
-our $VERSION = '1.0016';
+our $VERSION = '1.0017';
 $VERSION = eval $VERSION;
 
 use HTTP::Headers;
--- lib/Plack/Response.pm
+++ lib/Plack/Response.pm
@@ -1,7 +1,7 @@
 package Plack::Response;
 use strict;
 use warnings;
-our $VERSION = '1.0016';
+our $VERSION = '1.0017';
 $VERSION = eval $VERSION;
 
 use Plack::Util::Accessor qw(body status);
