d41582a847f4b3c6047f3f01595ee98a0e6083eb
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2013-08-22 14:06:23 -0700

    1.0029

--- lib/Plack.pm
+++ lib/Plack.pm
@@ -3,7 +3,7 @@ package Plack;
 use strict;
 use warnings;
 use 5.008_001;
-our $VERSION = '1.0028';
+our $VERSION = '1.0029';
 $VERSION = eval $VERSION;
 
 1;
--- lib/Plack/Response.pm
+++ lib/Plack/Response.pm
@@ -1,7 +1,7 @@
 package Plack::Response;
 use strict;
 use warnings;
-our $VERSION = '1.0028';
+our $VERSION = '1.0029';
 $VERSION = eval $VERSION;
 
 use Plack::Util::Accessor qw(body status);
