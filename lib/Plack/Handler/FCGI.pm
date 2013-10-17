plack/Plack

commit cafa5db84921f020183a9c834fd6a4541e5a6b84

Provide I/O handles to FCGI::Request() instead of relying on global STD{IN,OUT, ERR}, this to prevent clashes with IPC software such as Capture::Tiny.

lib/Plack/Handler/FCGI.pm
@@ -47,10 +47,13 @@ sub run {
         die "STDIN is not a socket: specify a listen location";
     }
 
+    @{$self}{qw(stdin stdout stderr)} 
+      = (IO::Handle->new, IO::Handle->new, IO::Handle->new);
+
     my %env;
     my $request = FCGI::Request(
-        \*STDIN, \*STDOUT,
-        ($self->{keep_stderr} ? \*STDOUT : \*STDERR), \%env, $sock,
+        $self->{stdin}, $self->{stdout},
+        ($self->{keep_stderr} ? $self->{stdout} : $self->{stderr}), \%env, $sock,
         ($self->{nointr} ? 0 : &FCGI::FAIL_ACCEPT_ON_INTR),
     );
 
@@ -93,9 +96,9 @@ sub run {
             %env,
             'psgi.version'      => [1,1],
             'psgi.url_scheme'   => ($env{HTTPS}||'off') =~ /^(?:on|1)$/i ? 'https' : 'http',
-            'psgi.input'        => *STDIN,
-            'psgi.errors'       => *STDERR, # FCGI.pm redirects STDERR in Accept() loop, so just print STDERR
-                                            # print to the correct error handle based on keep_stderr
+            'psgi.input'        => $self->{stdin},
+            'psgi.errors'       => $self->{stderr}, # FCGI.pm redirects STDERR in Accept() loop, so just print STDERR
+                                                    # print to the correct error handle based on keep_stderr
             'psgi.multithread'  => Plack::Util::FALSE,
             'psgi.multiprocess' => Plack::Util::TRUE,
             'psgi.run_once'     => Plack::Util::FALSE,
@@ -171,9 +174,9 @@ sub _handle_response {
     }
     $hdrs .= "\015\012";
 
-    print STDOUT $hdrs;
+    print { $self->{stdout} } $hdrs;
 
-    my $cb = sub { print STDOUT $_[0] };
+    my $cb = sub { print { $self->{stdout} } $_[0] };
     my $body = $res->[2];
     if (defined $body) {
         Plack::Util::foreach($body, $cb);

commit d29aed7897aa36f42b416a97b6d4afd5484ddba3

Restore --keep-stderr for Handler::FCGI.
Commit cafa5db broke --keep-stderr for Handler::FCGI: psgi.errors ended
up being set to an unopened filehandle, so errors disappeared into
nowhere.

The call to FCGI::Request was always wrong: that function behaves like
'open' in that it points the passed-in filehandles at the FCGI streams,
so it never makes sense to pass the same filehandle twice. If it had
done anything it would have copied the response to the error log; in
fact it did nothing.

Also change the documentation to reflect reality: --keep-stderr has
always send psgi.errors to STDERR, not STDOUT.

lib/Plack/Handler/FCGI.pm
@@ -53,8 +53,8 @@ sub run {
 
     my %env;
     my $request = FCGI::Request(
-        $self->{stdin}, $self->{stdout},
-        ($self->{keep_stderr} ? $self->{stdout} : $self->{stderr}), \%env, $sock,
+        $self->{stdin}, $self->{stdout}, $self->{stderr},
+        \%env, $sock,
         ($self->{nointr} ? 0 : &FCGI::FAIL_ACCEPT_ON_INTR),
     );
 
@@ -98,8 +98,8 @@ sub run {
             'psgi.version'      => [1,1],
             'psgi.url_scheme'   => ($env{HTTPS}||'off') =~ /^(?:on|1)$/i ? 'https' : 'http',
             'psgi.input'        => $self->{stdin},
-            'psgi.errors'       => $self->{stderr}, # FCGI.pm redirects STDERR in Accept() loop, so just print STDERR
-                                                    # print to the correct error handle based on keep_stderr
+            'psgi.errors'       => 
+                ($self->{keep_stderr} ? \*STDERR : $self->{stderr}),
             'psgi.multithread'  => Plack::Util::FALSE,
             'psgi.multiprocess' => Plack::Util::TRUE,
             'psgi.run_once'     => Plack::Util::FALSE,
@@ -282,7 +282,7 @@ Specify process title
 
 =item keep-stderr
 
-Send STDERR to STDOUT instead of the webserver
+Send psgi.errors to STDERR instead of to the FCGI error stream.
 
 =item backlog
 