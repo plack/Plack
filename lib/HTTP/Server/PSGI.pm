0b33e362aef23984432af8589c0a1a7f443fb65b
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2016-11-14 00:55:48 +0100

    Do not call `eval` in every request

--- lib/HTTP/Server/PSGI.pm
+++ lib/HTTP/Server/PSGI.pm
@@ -13,10 +13,12 @@ use Plack::Util;
 use Stream::Buffered;
 use Plack::Middleware::ContentLength;
 use POSIX qw(EINTR);
-use Socket qw(IPPROTO_TCP TCP_NODELAY);
+use Socket qw(IPPROTO_TCP);
 
 use Try::Tiny;
 use Time::HiRes qw(time);
+
+use constant TCP_NODELAY => try { Socket::TCP_NODELAY };
 
 my $alarm_interval;
 BEGIN {
@@ -105,7 +107,7 @@ sub accept_loop {
     while (1) {
         local $SIG{PIPE} = 'IGNORE';
         if (my $conn = $self->{listen_sock}->accept) {
-            if (eval { TCP_NODELAY }) {
+            if (defined TCP_NODELAY) {
                 $conn->setsockopt(IPPROTO_TCP, TCP_NODELAY, 1)
                     or die "setsockopt(TCP_NODELAY) failed:$!";
             }
