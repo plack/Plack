5d8dba30e974a53ca12a47e500809fb08fc1052b
Robert Rothenberg <rrwo@cpan.org>
2017-11-15 17:12:31 +0000

    Set Content-Length to 0 in XSendfile middleware #602
    Also add POD documenting the configuration options.

--- lib/Plack/Middleware/XSendfile.pm
+++ lib/Plack/Middleware/XSendfile.pm
@@ -25,10 +25,12 @@ sub call {
                     my $path = $body->path;
                     my $url = $self->map_accel_path($env, $path);
                     $h->set($type => $url) if $url;
+                    $h->set('Content-Length', 0);
                     $body = [];
                 } elsif ($type eq 'X-Sendfile' or $type eq 'X-Lighttpd-Send-File') {
                     my $path = $body->path;
                     $h->set($type => $path) if defined $path;
+                    $h->set('Content-Length', 0);
                     $body = [];
                 } else {
                     $env->{'psgi.errors'}->print("Unknown x-sendfile variation: $type");
@@ -70,13 +72,48 @@ Plack::Middleware::XSendfile - Sets X-Sendfile (or a like) header for frontends
 
 =head1 DESCRIPTION
 
+When the body is a blessed reference with a C<path> method, then the
+return value of that method is used to set the X-Sendfile header.
+
+The body is set to an empty list, and the Content-Length header is
+set to 0.
+
+If the X-Sendfile header is already set, then the body and
+Content-Length will be untouched.
+
 You should use L<IO::File::WithPath> or L<Plack::Util>'s
 C<set_io_path> to add C<path> method to an IO object in the body.
 
 See L<http://github.com/rack/rack-contrib/blob/master/lib/rack/contrib/sendfile.rb>
 for the frontend configuration.
 
-Plack::Middleware::XSendfile does not set the content-type header.
+Plack::Middleware::XSendfile does not set the Content-Type header.
+
+=head1 CONFIGURATION
+
+=over 4
+
+=item variation
+
+The header tag to use. If unset, the environment key
+C<plack.xsendfile.type> will be used, then the C<HTTP_X_SENDFILE_TYPE>
+header.
+
+Supported values are:
+
+=over
+
+=item * C<X-Accel-Redirect>
+
+=item * C<X-Lighttpd-Send-File>
+
+=item * C<X-Sendfile>.
+
+=back
+
+An unsupport value will log an error.
+
+=back
 
 =head1 AUTHOR
 
