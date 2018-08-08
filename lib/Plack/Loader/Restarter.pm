5f7c14c006021158942a9c7e5196b50dc6c956f9
Phil Carmody <phil@ratewhisky.org>
2018-08-08 11:47:14 +0300

    Restarter - expand validity regexp for readability, no functional change
    Signed-off-by: Phil Carmody <phil@ratewhisky.org>

--- lib/Plack/Loader/Restarter.pm
+++ lib/Plack/Loader/Restarter.pm
@@ -52,7 +52,12 @@ sub valid_file {
     if ( $file->{path} =~ m{(\d+)$} && $1 >= 4913 && $1 <= 5036) {
         return 0;
     }
-    $file->{path} !~ m!\.(?:git|svn)[/\\]|\.(?:bak|swp|swpx|swx)$|~$|_flymake\.p[lm]$|\.#!;
+    $file->{path} !~ m!\.(?:git|svn)[/\\]             # VCS repositories
+                      |\.(?:bak|swp|swpx|swx)$        # tmp/backup extensions
+                      |~$                             # ditto, not an extension
+                      |_flymake\.p[lm]$
+                      |\.#
+                      !x;
 }
 
 sub run {
