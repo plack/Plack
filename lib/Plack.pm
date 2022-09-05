7d4011216825529dadbbb304afd2477fcc4b28e3
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2022-09-05 08:47:12 -0700

    1.0050

--- lib/Plack.pm
+++ lib/Plack.pm
@@ -3,7 +3,7 @@ package Plack;
 use strict;
 use warnings;
 use 5.008_001;
-our $VERSION = '1.0049';
+our $VERSION = '1.0050';
 
 1;
 __END__
--- lib/Plack/Response.pm
+++ lib/Plack/Response.pm
@@ -1,7 +1,7 @@
 package Plack::Response;
 use strict;
 use warnings;
-our $VERSION = '1.0049';
+our $VERSION = '1.0050';
 
 use Plack::Util::Accessor qw(body status);
 use Carp ();
