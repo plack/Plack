5a52015232f6c9611c5519400afd5cd58fd9021a
Tatsuhiko Miyagawa <tmiyagawa@fastly.com>
2023-09-14 14:41:20 -0700

    use ref to avoid calling overloaded methods. Fix #697

--- lib/Plack/Middleware/StackTrace.pm
+++ lib/Plack/Middleware/StackTrace.pm
@@ -55,7 +55,8 @@ sub call {
         $trace = $caught_trace if $caught_trace;
     }
 
-    if ($trace && ($caught || ($self->force && ref $res eq 'ARRAY' && $res->[0] == 500)) ) {
+    # Use ref $trace to avoid calling as_string twice when it's not necessary
+    if (ref $trace && ($caught || ($self->force && ref $res eq 'ARRAY' && $res->[0] == 500)) ) {
         my $text = $trace->as_string;
         my $html = $trace->as_html;
         $env->{'plack.stacktrace.text'} = $text;
